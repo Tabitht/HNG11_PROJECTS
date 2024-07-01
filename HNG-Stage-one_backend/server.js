f127552c1f55e44161794d58c04bce760506ec02
Tabitht <imafidontabitha@gmail.com>
2024-07-01 16:47:03 +0100

    made temperature data dynamic

--- HNG-Stage-one_backend/server.js
+++ HNG-Stage-one_backend/server.js
@@ -3,20 +3,22 @@ const axios = require('axios');
 
 const app = express();
 app.set('trust proxy', true);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/api/hello', async (req, res) => {
   const visitorName = req.query.visitor_name;
   const clientIp = req.ip;
+  const apiKey = process.env.API_KEY
 
   try {
     const geoResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
     const city = geoResponse.data.city;
-
+    const weatherResponse = await axios.get(`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`);
+    const Temperature = weatherResponse.data.temperature.current
     res.json({
       client_ip: clientIp,
       location: city,
-      greeting: `Hello, ${visitorName}!, the temperature is 11 degrees Celsius in ${city}`
+      greeting: `Hello, ${visitorName}!, the ${Temperature} is 11 degrees Celsius in ${city}`
     });
   } catch (error) {
     res.status(500).json({ error: 'Failed to Get location' });
