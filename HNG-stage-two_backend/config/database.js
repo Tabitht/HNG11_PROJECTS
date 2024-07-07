63481f0bdc0f3db1732a99492b93a8b22f64d6bd
Tabitht <imafidontabitha@gmail.com>
2024-07-07 18:43:07 +0000

    made the url to be hosted on render

--- HNG-stage-two_backend/config/database.js
+++ HNG-stage-two_backend/config/database.js
@@ -3,9 +3,21 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const databaseUrl = process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(databaseUrl/**process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,**/ {
   host: process.env.DB_HOST,
   dialect: 'postgres',
+  dialectOptions:{
+	ssl: isProduction
+	  ? {
+          require: true,
+          rejectUnauthorized: false,
+        }
+	: false,
+  },
+  logging: false,
+});
 });
 
 module.exports = sequelize;
