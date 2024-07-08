f905902936679f97d6422de7911a48baf38db9f7
Tabitht <imafidontabitha@gmail.com>
2024-07-08 14:04:39 +0100

    minor changes

--- HNG-stage-two_backend/config/database.js
+++ HNG-stage-two_backend/config/database.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 // const databaseUrl = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(databaseUrl, {
+ // process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
 });
@@ -19,8 +20,5 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   },
   logging: false,**/
 // });
-
-/**process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,**/
-// databaseUrl
 
 module.exports = sequelize;
