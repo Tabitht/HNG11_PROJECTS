1950dc172f11b07f9ee0c9776f78344a2b51fde6
Tabitht <imafidontabitha@gmail.com>
2024-07-08 18:35:17 +0100

    made little changes

--- HNG-stage-two_backend/config/database.js
+++ HNG-stage-two_backend/config/database.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(//databaseUrl, {
-  process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(databaseUrl, {
+ // process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
 });
